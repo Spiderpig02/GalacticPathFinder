81cb7aaff695e7f2583e416965ed3b3a143c50b4
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-01-02 14:50:54 +0100

    feat: Add fetch_graph_heuristics post-method
    Add fetch_graph_heuristics post-method for fetching the available heuristics for a given graph traversal method selected by the user.

--- backend/graphtraversal/views.py
+++ backend/graphtraversal/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpRequest
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+from factory import get_heuristics
 
 
 @api_view(["POST"])
@@ -24,14 +25,19 @@ def post_graph_traversal(request):
         print(e)
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-@api_view(["GET"])
+
+@api_view(["POST"])
 @permission_classes([permissions.AllowAny])
-def get_graph_traversal_methods(request):
+def fetch_graph_heuristics(request) -> HttpResponse[list[str]]:
     """
-    This endpoint retrieves all legal graph traversal methods that this service provides.
+    This endpoint returns a list of available heuristics for a given graph traversal method
     """
     try:
-        print("Success")
+        heuristics: list[str] = get_heuristics(request.data["graph_method_name"])
+        return HttpResponse(data=heuristics, status=status.HTTP_200_OK)
+    except ValueError as va:
+        print(va)
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         print(e)
-        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
