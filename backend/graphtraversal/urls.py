0629ba1b46c1a727c03e0823903c6ff87e1092b9
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-01-02 14:22:07 +0100

    feat: :sparkles: Add post_graph_traversal HTTP-POST method in views.py
    Add post_graph_traversal HTTP-POST method in views.py

--- backend/graphtraversal/urls.py
+++ backend/graphtraversal/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('graphtraversal/', include('graphtraversal.urls'), name="graphtraversal"),
+    path('/traverse'),
+    path('/graph-traversal-methods'),
+    path('/graph-heuristics-methods'),
 ]
--- backend/graphtraversal/views.py
+++ backend/graphtraversal/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from .models import newProject
+from .serializers import NewProjectSerializer
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from .models import newProject
+from .serializers import NewProjectSerializer
+
+from .exceptions import IllegalArgumentError  # Import the IllegalArgumentError class
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def post_graph_traversal(request):
+    '''
+    This endpoint performs a graph traversal based on the provided algorithm,
+        heuristic (if applicable), start and end points, and the map
+    '''
+    try:
+        print("Success")
+    except IllegalArgumentError as IAe:
+        print(IAe)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        print(e)
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
