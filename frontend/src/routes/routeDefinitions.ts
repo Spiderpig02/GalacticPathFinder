f6e24dc6181b939bb528d42b7cabd9b17626c33b
Sverre Nystad <sverrenystad@gmail.com>
2024-01-02 12:32:22 +0100

    Feat: Add route definitions and API URLs

---
+++ frontend/src/routes/routeDefinitions.ts
@@ -0,0 +1,11 @@
+const baseAPIUrl : string = 'http://localhost:8080/api';
+
+const routes = {
+
+};
+
+const apiRoutes = {
+  graphTraversalNames: `${baseAPIUrl}/graph-traversal-methods`,
+};
+
+export { routes, apiRoutes };
