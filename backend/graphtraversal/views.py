e119d4cd4cf732e5f329e6db4353cac2a8186946
Sverre Nystad <sverrenystad@gmail.com>
2024-01-02 18:08:56 +0100

    Feat: implement graph traversal views

--- backend/graphtraversal/views.py
+++ backend/graphtraversal/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -12,8 +11,22 @@ def post_graph_traversal(request):
     This endpoint performs a graph traversal based on the provided algorithm,
         heuristic (if applicable), start and end points, and the map
     """
+
     try:
-        print("Success")
+        # Get mandatory parameters
+        graph_method_name: str = request.data["algorithm"]
+        start_point: list[int, int] = request.data["startPoint"]
+        end_point: list[int, int] = request.data["endPoint"]
+        map: list[list[int, int]] = request.data["map"]
+        print(
+            f"graph_method_name: {graph_method_name}, start_point: {start_point}, end_point: {end_point}, map: {map}"
+        )
+        # Get optional heuristic
+        heuristic: str = request.data.get("heuristic", None)
+
+        print(f"heuristic: {heuristic}")
+        return Response(status=status.HTTP_200_OK, data=get_graph_traversal_methods())
+
     except ValueError as va:
         print(va)
         return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -27,7 +40,6 @@ def fetch_graph_traversal_methods(request):
     """
     This endpoint retrieves all legal graph traversal methods that this service provides.
     """
-    print("GET fetch_graph_traversal_methods was called")
     try:
         return Response(status=status.HTTP_200_OK, data=get_graph_traversal_methods())
 
@@ -43,7 +55,6 @@ def fetch_graph_heuristics(request):
     """
     This endpoint returns a list of available heuristics for a given graph traversal method
     """
-    print("POST fetch_graph_heuristics was called")
     try:
         heuristics: list[str] = get_heuristics(request.data["graph_method_name"])
         return Response(status=status.HTTP_200_OK, data=heuristics)
