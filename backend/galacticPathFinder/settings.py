8c9841e9278f6cf6f272b1035587f136e41db3a5
Sverre Nystad <sverrenystad@gmail.com>
2024-01-02 20:25:55 +0100

    Feat: Add rest_framework_swagger to INSTALLED_APPS and templates paths

--- backend/galacticPathFinder/settings.py
+++ backend/galacticPathFinder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
+    "rest_framework_swagger",
     "graphtraversal",
 ]
 
@@ -60,7 +62,7 @@ ROOT_URLCONF = "galacticPathFinder.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
