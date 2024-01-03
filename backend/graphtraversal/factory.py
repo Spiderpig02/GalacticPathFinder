831039037b10c8c4428e5213b33dbb0589393cb3
Sverre Nystad <sverrenystad@gmail.com>
2024-01-03 01:41:33 +0100

    Feat: Add typing and refactor get_heuristics function

--- backend/graphtraversal/factory.py
+++ backend/graphtraversal/factory.py
@@ -1,4 +1,6 @@
 """ This module contains the factory functions for the graph traversal algorithms """
+from typing import Callable
+from graphtraversal.algorithms.heuristics import manhattan_distance
 from graphtraversal.algorithms.a_star import AStarPathfinder
 from graphtraversal.algorithms.pathfinder import Pathfinder
 
@@ -24,7 +26,6 @@ def get_graph_traversal_methods() -> list[str]:
     This function returns a list of all available graph traversal methods
     """
     method_names: list[str] = list(graph_traversal_function_map.keys())
-    print(f"method_names: {method_names}")
     return method_names
 
 
@@ -34,10 +35,21 @@ def get_heuristics(graph_method_name: str) -> list[str]:
     """
     match graph_method_name.lower():
         case "a star":
-            return ["euclidean", "manhattan"]
+            return ["manhattan", "euclidean"]
         case "dijkstra":
             return []
         case "bfs":
             return []
         case _:
             raise ValueError(f"Invalid graph method name: {graph_method_name}")
+
+
+def get_heuristic(heuristic_name: str) -> Callable:
+    """
+    This function returns the heuristic function for the given heuristic name
+    """
+    match heuristic_name.lower():
+        case "manhattan":
+            return manhattan_distance
+        case _:
+            raise ValueError(f"Invalid heuristic name: {heuristic_name}")
