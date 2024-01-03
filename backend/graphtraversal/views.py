b90609bae5f86918522238c55678296ab4613593
Sverre Nystad <sverrenystad@gmail.com>
2024-01-03 02:01:40 +0100

    Feat: Create graph traversal algorithm and map parsing returning mock data and handling bad input

--- backend/graphtraversal/views.py
+++ backend/graphtraversal/views.py
@@ -1,10 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.decorators import api_view
 from rest_framework import status
+import json
 
 from graphtraversal.algorithms.pathfinder import Pathfinder
-from graphtraversal.map import RestMap
+from graphtraversal.map import RestMap, Node, Position
 from graphtraversal.factory import (
     get_heuristics,
     get_graph_traversal_methods,
@@ -45,49 +45,85 @@ def post_graph_traversal(request):
             raise ValueError("Invalid request. Missing 'map' parameter")
 
         # Get mandatory parameters
-        graph_method_name: str = request.data["algorithm"]
-        start_point: list[int, int] = request.data["startPoint"]
-        end_point: list[int, int] = request.data["endPoint"]
-        map: list[list[int, int]] = request.data["map"]
+        algorithm: str = request.data["algorithm"]
+        start_point = request.data["startPoint"]
+        end_point = request.data["endPoint"]
+        raw_map = request.data["map"]
 
-        # Parse data
-        graph_method_name = (graph_method_name.lower()).strip()
-
-        # Get optional heuristic
-        heuristic: str = request.data.get("heuristic", None)
-
-        # Create a map object
-        map = RestMap(map, start_point, end_point)
         # Check if the algorithm name is valid
-        if graph_method_name not in get_graph_traversal_methods():
+        if algorithm not in get_graph_traversal_methods():
             raise ValueError(
-                f"Invalid algorithm name {graph_method_name} Should be one of {str(get_graph_traversal_methods())}"
+                f"Invalid algorithm name {algorithm} Should be one of {str(get_graph_traversal_methods())}"
             )
 
-        if heuristic not in get_heuristics(graph_method_name):
+        # Parse data
+        algorithm: str = (algorithm.lower()).strip()
+
+        start_point: Node = Node(
+            Position(int(start_point.get("x", 0)), int(start_point.get("y", 0))),
+            float(start_point.get("weight", 0)),
+        )
+
+        end_point: Node = Node(
+            Position(int(end_point.get("x", 0)), int(end_point.get("y", 0))),
+            float(end_point.get("weight", 0)),
+        )
+
+        nodes = []
+        for node in raw_map:
+            x = int(node.get("x", 0))
+            y = int(node.get("y", 0))
+            weight = float(node.get("weight", 0))
+
+            nodes.append(Node(Position(x, y), weight))
+
+        map: RestMap = RestMap(
+            nodes,
+            start_point,
+            end_point,
+        )
+
+        # Get optional heuristic and check if it is valid
+        heuristic: str = request.data.get("heuristic", None)
+        if heuristic not in get_heuristics(algorithm):
             heuristic = None
 
         # Find path
-        pathfinder: Pathfinder = get_pathfinder(graph_method_name)
+        pathfinder: Pathfinder = get_pathfinder(algorithm)
         print(f"pathfinder: {pathfinder}")
         # path, node_order = pathfinder.find_path(map, start_point, end_point, heuristic)
 
         # pathfinder_status: str = "success" if path is not None else "failure"
 
+        # show the parsed data
+        print(f"algorithm: {algorithm}")
+        print(f"start_point: {start_point}")
+        print(f"end_point: {end_point}")
+        print(f"map: {map}")
+
         ##### CREATE MOCK DATA #####
         pathfinder_status = "success"
-        path = [[1, 1], [1, 2], [1, 3], [2, 3], [3, 3]]
-        node_order = [[1, 1], [1, 2], [1, 3], [2, 3], [3, 3]]
+        # Serialize the list of dictionaries to a JSON string
+        path = [node.to_dict() for node in nodes]
+        serialized_path = json.dumps(path)
+
+        node_order = [node.to_dict() for node in nodes]
+        serialized_node_order = json.dumps(node_order)
         return Response(
             status=status.HTTP_200_OK,
-            data={"message": pathfinder_status, "path": path, "nodeOrder": node_order},
+            data={
+                "message": pathfinder_status,
+                "path": serialized_path,
+                "nodeOrder": serialized_node_order,
+            },
         )
 
     except ValueError as va:
         print(va)
         return Response(status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
-        print(e)
+        print("====================================")
+        print("Exception: ", e)
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
