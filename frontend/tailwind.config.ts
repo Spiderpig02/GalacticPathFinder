c51a4dc8d1390f45d2466c5512b193684b7fca2e
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2024-01-21 19:35:41 +0100

    style: :lipstick: Add colors to tailwind config
    Add colors to tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,7 +4,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ss-navy-blue": "#05445e",
+        "ss-blue-green": "#75e6da",
+        "wt-midnight-blue": "#41729f",
+        "ss-baby-blue": "#d4f1f4",
+        "t-white": "#f7f8f9",
+        "ss-blue-grotto": "#189ab4",
+        black: "#000000",
+        "ml-dark-blue": "#0c2d48",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
