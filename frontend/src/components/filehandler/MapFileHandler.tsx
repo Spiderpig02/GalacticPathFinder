2549f702b0112ddbcae77f7e64857ff805bb7d01
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-01-22 00:11:47 +0100

    Feat: Add correct color

--- frontend/src/components/filehandler/MapFileHandler.tsx
+++ frontend/src/components/filehandler/MapFileHandler.tsx
@@ -1,13 +1,18 @@
+import { downloadMap, uploadMap } from "../../services/mapHandling";
 
 
 const MapHandler = () => {
     const handleUpload = () => {
         // Implement upload logic
+        // Get file from user
+        let csv_raw: string = ""
+
         console.log("Upload");
     };
 
     const handleDownload = () => {
         // Implement download logic
+        downloadMap();
         console.log("Download");
     };
     
@@ -15,12 +20,12 @@ const MapHandler = () => {
         <>
             <p className=" text-t-white">Download or upload map</p>
             <div className="flex flex-row justify-center items-center">
-                <button onClick={handleUpload} className="button-circle">
+                <button onClick={handleUpload} className="button-circle bg-ss-blue-grotto">
                     <svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.16895 31.5109V36.5109C1.16895 37.8369 1.69573 39.1087 2.63341 40.0464C3.57109 40.9841 4.84286 41.5109 6.16895 41.5109H36.1689C37.495 41.5109 38.7668 40.9841 39.7045 40.0464C40.6422 39.1087 41.1689 37.8369 41.1689 36.5109V31.5109M11.1689 11.5109L21.1689 1.51086M21.1689 1.51086L31.1689 11.5109M21.1689 1.51086V31.5109" stroke="#0C2D48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </button>
-                <button onClick={handleDownload} className="button-circle">
+                <button onClick={handleDownload} className="button-circle bg-ss-blue-grotto">
                     <div className=" br-rado"></div>
                     <svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.16895 31.5109V36.5109C1.16895 37.8369 1.69573 39.1087 2.63341 40.0464C3.57109 40.9841 4.84286 41.5109 6.16895 41.5109H36.1689C37.495 41.5109 38.7668 40.9841 39.7045 40.0464C40.6422 39.1087 41.1689 37.8369 41.1689 36.5109V31.5109M31.1689 21.5109L21.1689 31.5109M21.1689 31.5109L11.1689 21.5109M21.1689 31.5109V1.51086" stroke="#0C2D48" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
