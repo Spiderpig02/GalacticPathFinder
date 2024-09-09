9b820304e910c680867c7df50d3e960fc9b3f8a8
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-09-09 18:45:23 +0200

    feat: :sparkles: Added Loader-icon to Startbutton which indicates when the traversal is loading to give the user better feedback.
    #86

--- frontend/src/components/startButton/StartButton.tsx
+++ frontend/src/components/startButton/StartButton.tsx
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./StartButton.css";
 import { handleTraverse } from "../../services/animationService";
 import { handleClearEarlierTraversals } from "../../services/draw";
 
 const StartButton: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const startAnimation = () => {
+    setIsLoading(true);
     handleClearEarlierTraversals();
-    handleTraverse();
+    handleTraverse(() => {
+      setIsLoading(false);
+    });
   };
 
   return (
     <div className="start-button-container">
       <button className="start-button" onClick={startAnimation}>
-        <svg
-          width="64"
-          height="64"
-          viewBox="0 0 64 64"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="play-icon"
-        >
-          <path d="M26 20L46 32L26 44V20Z" fill="currentColor" />
-        </svg>
+        {isLoading ? (
+          <div className="loading-spinner"></div>
+        ) : (
+          <svg
+            width="64"
+            height="64"
+            viewBox="0 0 64 64"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="play-icon"
+          >
+            <path d="M26 20L46 32L26 44V20Z" fill="currentColor" />
+          </svg>
+        )}
       </button>
     </div>
   );
