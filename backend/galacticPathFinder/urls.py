1e299d863149bf00ecaf0a5aa39ec114e45032e4
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-09-09 22:21:04 +0200

    fix: :bug: Fixed bug where it was not possible to reload on any page other then the HomePage without being sent the "404 Not Found"-page
    #99

--- backend/galacticPathFinder/urls.py
+++ backend/galacticPathFinder/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -46,5 +47,7 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc")
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # Catch-all route to serve the React app's index.html
+    re_path(r"^(?!admin|graphtraversal|swagger|redoc).*", TemplateView.as_view(template_name="index.html")),
 ]
