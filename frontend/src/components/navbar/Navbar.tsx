92d5633371eac222b544ea9bc2e703fa0f95b9c5
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-12-06 19:50:02 +0100

    style: :lipstick: Made sure Navbar has solid background-color on AlgorithmsPage as well so that menu-options are visible when scrolling.
    #134

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -21,7 +21,10 @@ const Navbar: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    if (location.pathname === "/about-us") {
+    if (
+      location.pathname === "/about-us" ||
+      location.pathname === "/algorithms"
+    ) {
       setShouldHaveSolidColour(true);
     } else {
       setShouldHaveSolidColour(false);
