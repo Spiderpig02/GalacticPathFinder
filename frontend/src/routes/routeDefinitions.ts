b7a16371f80f0de7a7386fbdf14655f49d6babcc
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2025-09-14 17:25:10 +0200

    REMEBER TO CHANGE BACK

--- frontend/src/routes/routeDefinitions.ts
+++ frontend/src/routes/routeDefinitions.ts
@@ -1,6 +1,5 @@
 const baseAPIUrl: string =
-  (import.meta.env.VITE_BACKEND_URL as string) ??
-  "https://backend.galacticpathfinder.com";
+  (import.meta.env.VITE_BACKEND_URL as string) ?? "http://127.0.0.1:8000";
 
 const routes = {};
 
